7b189f22f4e75024f20bb5608e6b6bff3d5218da
Leonardo Bonacci <aabcehmu@mailfence.com>
2023-01-28 11:30:36 +1300

    tringress generated

---
+++ tringress/src/test/java/guru/bonacci5/tringress/TringressApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.bonacci5.tringress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TringressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
